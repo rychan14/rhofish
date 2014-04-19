f2ee5586f4e42615081182125a98e1417fc57987
rychan14 <rychan14@gmail.com>
2014-04-18 21:15:15 -0700

    committing

--- app.js
+++ app.js
@@ -4,10 +4,29 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
 var app = express();
+var graph = require('fbgraph')
+var twit = require('twit')
+var dotenv = require ('dotenv');
+dotenv.load();
 
 //route files to load
 var index = require('./routes/index');
 
+//add facebook-node-sdk
+var FB = require('fb');
+
+FB.api('oauth/access_token', {
+    client_id: '1392579854355462',
+    client_secret: '49d6f52555426eb22ab686e3ccb1037f',
+    grant_type: 'client_credentials'
+}, function (res) {
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+
+var accessToken = res.access_token;
+});
 //database setup - uncomment to set up your database
 //var mongoose = require('mongoose');
 //mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/DATABASE1);
@@ -21,6 +40,8 @@ app.use(express.bodyParser());
 
 //routes
 app.get('/', index.view);
+
+
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
 http.createServer(app).listen(app.get('port'), function(){
